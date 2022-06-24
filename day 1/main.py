d64b301feabbc9faceb5f9141af6a58092d33f8a
gee-0 <geemingchan2006@gmail.com>
2022-06-23 20:28:39 -0400

    finished day 1 of advent

---
+++ day 1/main.py
@@ -0,0 +1,20 @@
+# read from input file and store in a list
+
+def read_file(filename):
+    with open(filename, 'r') as f:
+        return f.read().splitlines()
+
+input = read_file('input.txt')
+
+# print(input)
+
+
+# calculate how many times the next number is greater than the previous from input
+
+count = 0
+for i in range(len(input)):
+    if i == 0:
+        continue
+    if int(input[i]) > int(input[i-1]):
+        count += 1
+print(count)
